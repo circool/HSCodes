b38b409849164f59fba1eb6cb366d57a900e8e30
Vladimir <circool@mail.ru>
2021-05-21 14:45:00 +0300

    Класс FileCheck проверяет наличие файла и возвращает количество его строк

---
+++ src/ru/tsurkanenko/vladimir/hscodes/FileCheck.java
@@ -0,0 +1,43 @@
+package ru.tsurkanenko.vladimir.hscodes;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Класс для проверки наличия внешнего файла и его размера
+ */
+class FileCheck {
+    /**
+     * Статический метод, возвращающий количество строк файла
+     * @param fileName Имя проверяемого файла
+     * @return Количество строк
+     */
+    static int getSize(String fileName){
+        int lineCounter = 0;
+        try (BufferedReader sourceFile =
+                     new BufferedReader(new FileReader(fileName))) {
+            while (sourceFile.readLine() != null)
+                lineCounter++;
+        } catch (IOException exception) {
+            System.out.println("Ошибка чтения файла " + fileName);
+        }
+        return lineCounter;
+    }
+
+    /**
+     * Статический метод, возвращающий наличие определенного в параметре файла
+     * @param fileName Имя проверяемого файла
+     * @return Наличие файла
+     */
+    static boolean isExist(String fileName){
+        boolean exist;
+        try (BufferedReader sourceFile =
+                     new BufferedReader(new FileReader(fileName))) {
+            exist = true;
+        } catch (IOException exception) {
+            exist = false;
+        }
+        return exist;
+    }
+}
