7bfbfdee25d8655c953e4a0d9b7d8ceb9ed22739
Vladimir <circool@mail.ru>
2021-05-22 23:46:01 +0300

    Проверка класса HSBase

---
+++ src/ru/tsurkanenko/vladimir/hscodes/TestHSBase.java
@@ -0,0 +1,16 @@
+package ru.tsurkanenko.vladimir.hscodes;
+
+public class TestHSBase {
+    public static void main(String[] args) {
+        HSBase demo = new HSBase();
+        int sect = 0;
+
+        System.out.println(demo.getSection().getCode(sect));
+        for(String codes:demo.getGroup().getCode())
+            if(codes.matches(("^" + demo.getSection().getCode(sect)) + ".*")) {
+                System.out.println(codes);
+                System.out.println(demo.getGroup().getDescription());
+            }
+
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,5 @@
 package sample;
-
+/*
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,3 +21,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+*/
