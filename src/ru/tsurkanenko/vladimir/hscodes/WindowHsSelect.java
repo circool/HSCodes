157317b0439c2478a3837d2fc9874daeb92caa26
Vladimir <circool@mail.ru>
2021-05-24 02:51:55 +0300

    Настройка реакции на изменение раздела или группы

--- src/ru/tsurkanenko/vladimir/hscodes/WindowHsSelect.java
+++ src/ru/tsurkanenko/vladimir/hscodes/WindowHsSelect.java
@@ -4,8 +4,6 @@ package ru.tsurkanenko.vladimir.hscodes;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
@@ -20,17 +18,18 @@ public class WindowHsSelect extends Application {
     public void start(Stage primaryStage) {
         HSBase hs = new HSBase();
         primaryStage.setTitle("Выбор кода ТНВЭД");
-        FlowPane root = new FlowPane(50,50);
+        FlowPane root = new FlowPane(50,10);
         primaryStage.setScene(new Scene(root, 1120, 600));
         root.setPadding(new Insets(10));
 
         Label sectionDescription = new Label("Примечания к разделу");
         sectionDescription.setWrapText(true);
-        sectionDescription.setMaxWidth(1100);
+        sectionDescription.setMaxSize(1100,300);
 
         Label groupDescription = new Label("Примечания к группе");
         groupDescription.setWrapText(true);
         groupDescription.setMaxWidth(1100);
+        groupDescription.setMaxHeight(300);
 
         // Выпадающий список Раздел
 
@@ -44,14 +43,32 @@ public class WindowHsSelect extends Application {
         groupCB.setMaxWidth(1100);
 
 
-        sectionCB.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // Отобразить примечание к разделу
-                sectionDescription.setText(hs.getSection().getNote(sectionCB.getValue().substring(0, 2))[0]);
+
+        sectionCB.setOnAction(event -> {
+            // Отобразить примечание к разделу
+            sectionDescription.setText(hs.getSection().getNote(sectionCB.getValue().substring(0, 2))[0]);
+            // выбрать первую из относяхщихся к разделу группы
+            String st = sectionCB.getValue().substring(0,2);
+            for (String singleLine:hsGroup) {
+                if(singleLine.substring(0,2).equals(st)) {
+                    groupCB.setValue(singleLine);
+                    break;
+                }
             }
         });
-        groupCB.setOnAction(event -> groupDescription.setText(hs.getGroup().getNote(groupCB.getValue().substring(0, 4))[0]));
+
+        groupCB.setOnAction(event -> {
+            // отобразить примечание к группе
+            groupDescription.setText(hs.getGroup().getNote(groupCB.getValue().substring(0, 4))[0]);
+            // выбрать раздел, к которому относится выбранная группа
+            String st = groupCB.getValue().substring(0,2);
+            for (String singleLine:hsSection) {
+                if(singleLine.substring(0,2).equals(st)) {
+                    sectionCB.setValue(singleLine);
+                    break;
+                }
+            }
+        });
 
 
         root.getChildren().addAll(sectionCB,sectionDescription,groupCB,groupDescription);
