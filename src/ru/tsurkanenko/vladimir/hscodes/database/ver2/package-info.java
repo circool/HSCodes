b52a47a07c32aaa58f372098adc45369fb9d108d
Vladimir <circool@mail.ru>
2021-06-22 00:24:02 +0300

    Рефакторинг проекта. Улаление устаревших классов

--- src/ru/tsurkanenko/vladimir/hscodes/database/ver2/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Этот вариант пакета ru.tsurkanenko.vladimir.hscodes.database использует символьные массивы для хранения строковых данных.
- */
-package ru.tsurkanenko.vladimir.hscodes.database.ver2;
